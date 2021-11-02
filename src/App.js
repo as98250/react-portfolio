d976b67639a99a3dc98e40f7b438c61771fa2372
Ariell SCott <scott.ariell00@gmail.com>
2021-11-01 22:11:16 -0400

    updated app.js and imported react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
-import React from "react";
-import Portfolio from "./components/Portfolio";
+import React, { Component } from 'react';
+import Header from './components/Header';
+import About from './components/About';
+import Resume from './components/Resume';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
-const App = () => <Portfolio />;
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <About />
+        <Resume />
+        <Portfolio />
+        <Contact />
+        <Footer />
+      </div>
+    );
+  }
+}
 
 export default App;
